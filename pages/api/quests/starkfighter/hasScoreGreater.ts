694b6d9c0d403da2b2e49fced263b30102c37ec2
Iris <iris.devillars@gmail.com>
2023-04-26 11:33:05 -0300

    feat: add starkfighter api requests

---
+++ pages/api/quests/starkfighter/hasScoreGreater.ts
@@ -0,0 +1,66 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type requestResponse = {
+  res: boolean;
+  error_msg?: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<requestResponse>
+) {
+  const {
+    query: { address, score },
+  } = req;
+
+  if (!address || Array.isArray(address)) {
+    return res
+      .status(400)
+      .json({ res: false, error_msg: "Invalid address parameter" });
+  }
+
+  if (!score) {
+    return res
+      .status(400)
+      .json({ res: false, error_msg: "Invalid score parameter" });
+  }
+
+  try {
+    if (address) {
+      const response = await fetch(
+        "https://server.starkfighter.xyz/fetch_user_score",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            user_addr: address,
+          }),
+        }
+      );
+
+      if (response.ok) {
+        const playerScore = await response.json();
+        if (playerScore && playerScore.score >= score) {
+          res
+            .setHeader("cache-control", "max-age=30")
+            .status(200)
+            .json({ res: true });
+        } else {
+          res
+            .status(400)
+            .json({ res: false, error_msg: "User has a score lower" });
+        }
+      } else {
+        res.status(400).json({ res: false, error_msg: "User has not played" });
+      }
+    } else {
+      res.status(400).json({ res: false, error_msg: "Invalid request" });
+    }
+  } catch (error: any) {
+    res
+      .status(error.status || 500)
+      .json({ res: false, error_msg: error.message });
+  }
+}
