9aeefbe5b1dd7d7766679acc743f94619596c536
Thomas Marchand <thomas.marchand@tuta.io>
2023-04-30 18:41:17 +0200

    feat: prettify api

--- pages/api/get_completed_tasks.ts
+++ pages/api/get_completed_tasks.ts
@@ -3,44 +3,50 @@ import NextCors from "nextjs-cors";
 import { connectToDatabase } from "../../lib/mongodb";
 
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse<{
-        task_ids: Number[]
-    } | QueryError>
+  req: NextApiRequest,
+  res: NextApiResponse<
+    | {
+        task_ids: Number[];
+      }
+    | QueryError
+  >
 ) {
-    await NextCors(req, res, {
-        methods: ["GET"],
-        origin: "*",
-        optionsSuccessStatus: 200,
-    });
+  await NextCors(req, res, {
+    methods: ["GET"],
+    origin: "*",
+    optionsSuccessStatus: 200,
+  });
 
-    const { db } = await connectToDatabase();
-    const {
-        query: { addr },
-    } = req;
-    try {
-        const completedTasks = await db.collection("completed_tasks").find({
-            address: addr
-        }).toArray();
-        if (completedTasks.length > 0) {
-            const tasksFormatted = completedTasks.map((quest) => {
-                const { task_id } = quest;
-                return task_id as Number;
-            });
-            res
-                .setHeader("cache-control", "max-age=30")
-                .status(200)
-                .json({ task_ids: tasksFormatted });
-        } else {
-            res
-                .setHeader("cache-control", "max-age=30")
-                .status(200)
-                .json({ error: "No completed tasks found for this user" });
-        }
-    } catch (error) {
-        res
-            .setHeader("cache-control", "max-age=30")
-            .status(500)
-            .json({ error: "Error querying completed tasks" });
+  const { db } = await connectToDatabase();
+  const {
+    query: { addr },
+  } = req;
+  try {
+    const completedTasks = await db
+      .collection("completed_tasks")
+      .find({
+        address: addr,
+      })
+      .toArray();
+    if (completedTasks.length > 0) {
+      const tasksFormatted = completedTasks.map((quest) => {
+        const { task_id } = quest;
+        return task_id as Number;
+      });
+      res
+        .setHeader("cache-control", "max-age=30")
+        .status(200)
+        .json({ task_ids: tasksFormatted });
+    } else {
+      res
+        .setHeader("cache-control", "max-age=30")
+        .status(200)
+        .json({ error: "No completed tasks found for this user" });
     }
+  } catch (error) {
+    res
+      .setHeader("cache-control", "max-age=30")
+      .status(500)
+      .json({ error: "Error querying completed tasks" });
+  }
 }
--- pages/api/get_tasks.ts
+++ pages/api/get_tasks.ts
@@ -3,42 +3,45 @@ import NextCors from "nextjs-cors";
 import { connectToDatabase } from "../../lib/mongodb";
 
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse<TaskDocument[] | QueryError>
+  req: NextApiRequest,
+  res: NextApiResponse<TaskDocument[] | QueryError>
 ) {
-    await NextCors(req, res, {
-        methods: ["GET"],
-        origin: "*",
-        optionsSuccessStatus: 200,
-    });
+  await NextCors(req, res, {
+    methods: ["GET"],
+    origin: "*",
+    optionsSuccessStatus: 200,
+  });
 
-    const { db } = await connectToDatabase();
-    const {
-        query: { quest_id },
-    } = req;
-    try {
-        const tasks = await db.collection("tasks").find({
-            quest_id: Number(quest_id)
-        }).toArray();
-        if (tasks.length > 0) {
-            const tasksFormatted = tasks.map((quest) => {
-                const { _id, ...rest } = quest;
-                return rest as TaskDocument;
-            });
-            res
-                .setHeader("cache-control", "max-age=30")
-                .status(200)
-                .json(tasksFormatted);
-        } else {
-            res
-                .setHeader("cache-control", "max-age=30")
-                .status(200)
-                .json({ error: "No tasks found for this quest_id" });
-        }
-    } catch (error) {
-        res
-            .setHeader("cache-control", "max-age=30")
-            .status(500)
-            .json({ error: "Error querying tasks" });
+  const { db } = await connectToDatabase();
+  const {
+    query: { quest_id },
+  } = req;
+  try {
+    const tasks = await db
+      .collection("tasks")
+      .find({
+        quest_id: Number(quest_id),
+      })
+      .toArray();
+    if (tasks.length > 0) {
+      const tasksFormatted = tasks.map((quest) => {
+        const { _id, ...rest } = quest;
+        return rest as TaskDocument;
+      });
+      res
+        .setHeader("cache-control", "max-age=30")
+        .status(200)
+        .json(tasksFormatted);
+    } else {
+      res
+        .setHeader("cache-control", "max-age=30")
+        .status(200)
+        .json({ error: "No tasks found for this quest_id" });
     }
+  } catch (error) {
+    res
+      .setHeader("cache-control", "max-age=30")
+      .status(500)
+      .json({ error: "Error querying tasks" });
+  }
 }
