93308f04f826b8fb40733bb1e6401cb2114fe66c
Thomas Marchand <thomas.marchand@tuta.io>
2023-04-30 18:38:19 +0200

    feat: add new endpoints for quests

---
+++ pages/api/get_completed_tasks.ts
@@ -0,0 +1,44 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import NextCors from "nextjs-cors";
+import { connectToDatabase } from "../../lib/mongodb";
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Number[] | QueryError>
+) {
+    await NextCors(req, res, {
+        methods: ["GET"],
+        origin: "*",
+        optionsSuccessStatus: 200,
+    });
+
+    const { db } = await connectToDatabase();
+    const {
+        query: { addr },
+    } = req;
+    try {
+        const completedTasks = await db.collection("completed_tasks").find({
+            address: addr
+        }).toArray();
+        if (completedTasks.length > 0) {
+            const tasksFormatted = completedTasks.map((quest) => {
+                const { task_id } = quest;
+                return task_id as Number;
+            });
+            res
+                .setHeader("cache-control", "max-age=30")
+                .status(200)
+                .json(tasksFormatted);
+        } else {
+            res
+                .setHeader("cache-control", "max-age=30")
+                .status(200)
+                .json({ error: "No completed tasks found for this user" });
+        }
+    } catch (error) {
+        res
+            .setHeader("cache-control", "max-age=30")
+            .status(500)
+            .json({ error: "Error querying completed tasks" });
+    }
+}
---
+++ pages/api/get_experience.ts
@@ -0,0 +1,45 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import NextCors from "nextjs-cors";
+import { connectToDatabase } from "../../lib/mongodb";
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<{
+        experience: number;
+    } | QueryError>
+) {
+    await NextCors(req, res, {
+        methods: ["GET"],
+        origin: "*",
+        optionsSuccessStatus: 200,
+    });
+
+    const { db } = await connectToDatabase();
+    const {
+        query: { addr },
+    } = req;
+    try {
+        const user = await db.collection("users").findOne({
+            address: addr
+        });
+        if (user) {
+            res
+                .setHeader("cache-control", "max-age=30")
+                .status(200)
+                .json({
+                    experience: user.exp
+                });
+        } else {
+            res
+                .setHeader("cache-control", "max-age=30")
+                .status(200)
+                .json({ error: "No user found matching this addr" });
+        }
+
+    } catch (error) {
+        res
+            .setHeader("cache-control", "max-age=30")
+            .status(500)
+            .json({ error: "Error querying experience" });
+    }
+}
