0dd9a1d8675917a5cc576e8ad6e7513e83bc0b06
Fricoben <benjamin.flores.pro@gmail.com>
2023-07-01 15:32:52 +0200

    feat: add confettis

--- styles/theme.ts
+++ styles/theme.ts
@@ -3,16 +3,14 @@ import { createTheme } from "@mui/material/styles";
 const theme = createTheme({
   palette: {
     primary: {
-      light: "#19AA6E",
-      main: "#19AA6E",
-      dark: "#19AA6E",
-      contrastText: "#19AA6E",
+      light: "#6affaf",
+      main: "#6affaf",
+      dark: "#6affaf",
     },
     secondary: {
-      light: "#BF9E7B",
-      main: "#BF9E7B",
-      dark: "#BF9E7B",
-      contrastText: "#000",
+      light: "#6affaf",
+      main: "#6affaf",
+      dark: "#6affaf",
     },
   },
 });
