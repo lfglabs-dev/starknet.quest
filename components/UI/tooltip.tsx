98fb80d99d2dcadde045e582397cca57ea11d5da
Iris <iris.devillars@gmail.com>
2023-08-31 11:42:25 +0200

    fix: build error

--- components/UI/tooltip.tsx
+++ components/UI/tooltip.tsx
@@ -1,9 +1,10 @@
+import React from "react";
 import styled from "@emotion/styled";
 import { Tooltip, TooltipProps, tooltipClasses } from "@mui/material";
 
 export const CustomTooltip = styled(({ className, ...props }: TooltipProps) => (
   <Tooltip {...props} classes={{ popper: className }} />
-))(({ theme }) => ({
+))(() => ({
   [`& .${tooltipClasses.tooltip}`]: {
     backgroundColor: "#191527",
     borderRadius: "12px",
