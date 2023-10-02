def996c462b1be2356aedafbe89c1b635e2b7b5e
Iris <iris.devillars@gmail.com>
2023-10-02 12:05:28 +0200

    fix: achievement skeleton

--- components/skeletons/achievementSkeleton.tsx
+++ components/skeletons/achievementSkeleton.tsx
@@ -1,27 +1,31 @@
 import React, { FunctionComponent } from "react";
-import { Skeleton } from "@mui/material";
-import styles from "../../styles/achievements.module.css";
+import { Skeleton, useMediaQuery } from "@mui/material";
 
 const AchievementSkeleton: FunctionComponent = () => {
+  const isMobile = useMediaQuery("(max-width:768px)");
   return (
     <>
       <Skeleton
         variant="rounded"
-        width={650}
-        height={245}
+        width={1000}
+        height={isMobile ? 400 : 245}
         sx={{
           bgcolor: "grey.900",
           marginBottom: "40px",
           borderRadius: "30px",
+          maxWidth: `${isMobile ? "100%" : "min(1500px, calc(100% - 32px))"}`,
+          margin: "0 auto 40px",
         }}
       />
       <Skeleton
         variant="rounded"
-        width={650}
-        height={245}
+        width={1000}
+        height={isMobile ? 400 : 245}
         sx={{
           bgcolor: "grey.900",
           borderRadius: "30px",
+          maxWidth: `${isMobile ? "100%" : "min(1500px, calc(100% - 32px))"}`,
+          margin: "0 auto",
         }}
       />
     </>
