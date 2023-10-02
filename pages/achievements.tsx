64fc75eb7aada02487190c5e64954e40448ddbf7
Iris <iris.devillars@gmail.com>
2023-10-02 18:33:24 +0200

    dev: try without location

--- pages/achievements.tsx
+++ pages/achievements.tsx
@@ -13,7 +13,7 @@ import AchievementSkeleton from "../components/skeletons/achievementSkeleton";
 import { useLocation } from "react-use";
 
 const Achievements: NextPage = () => {
-  const location = useLocation();
+  // const location = useLocation();
   const { address } = useAccount();
   const [userAchievements, setUserAchievements] = useState<
     AchievementsDocument[]
@@ -59,7 +59,8 @@ const Achievements: NextPage = () => {
       clearTimeout(timer);
       setHasChecked(false);
     };
-  }, [location, address]);
+  }, [address]);
+  // location,
 
   // Map through user achievements and check if any are completed
   useEffect(() => {
