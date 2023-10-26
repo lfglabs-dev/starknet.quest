660d36f987d23fef5067203994d4a6f4beed7524
Iris <iris.devillars@gmail.com>
2023-10-26 17:11:12 +0200

    fix: remove logs

--- hooks/useNotificationManager.ts
+++ hooks/useNotificationManager.ts
@@ -23,7 +23,6 @@ export function useNotificationManager() {
   useEffect(() => {
     const intervalId = setInterval(() => {
       notifications.forEach(checkTransactionStatus);
-      console.log("notifications", notifications);
     }, 5000);
 
     return () => clearInterval(intervalId); // Cleanup on component unmount
