0e8f3b2f8f186eb736840a7b62d362935a3d88ed
Iris <iris.devillars@gmail.com>
2023-11-01 15:22:02 +0100

    fix: build error

--- pages/quest/[questPage].tsx
+++ pages/quest/[questPage].tsx
@@ -16,36 +16,10 @@ import BannerPopup from "../../components/UI/menus/bannerPopup";
 import { useDomainFromAddress } from "../../hooks/naming";
 import Head from "next/head";
 
-// type QuestPageProps = {
-//   customTags: boolean;
-//   questTags?: QuestDocument;
-// };
-
-type Props = {
-  params: { id: string };
-  searchParams: { [key: string]: string | string[] | undefined };
+type QuestPageProps = {
+  customTags: boolean;
+  questTags?: QuestDocument;
 };
-
-export async function generateMetadata(
-  { params, searchParams }: Props,
-  parent: ResolvingMetadata
-): Promise<Metadata> {
-  // read route params
-  const id = params.id;
-
-  // fetch data
-  const product = await fetch(`https://.../${id}`).then((res) => res.json());
-
-  // optionally access and extend (rather than replace) parent metadata
-  const previousImages = (await parent).openGraph?.images || [];
-
-  return {
-    title: product.title,
-    openGraph: {
-      images: ["/some-specific-page-image.jpg", ...previousImages],
-    },
-  };
-}
 
 /* eslint-disable react/prop-types */
 const QuestPage: NextPage<QuestPageProps> = ({ customTags, questTags }) => {
