181381ef3bb35595d26a0dd8fb335cc1f30e09fc
ayushtom <tomar2585@gmail.com>
2023-11-08 14:26:44 +0530

    chore: fix build issue

--- components/leaderboard/searchbar.tsx
+++ components/leaderboard/searchbar.tsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { FunctionComponent } from "react";
 import SearchIcon from "../../public/icons/searchIcon.svg";
 import Image from "next/image";
 
-export default function Searchbar({ handleSearch, value, onKeyDown }) {
+type SearchbarProps = {
+  handleSearch: (_: string) => void;
+  value: string;
+  onKeyDown: (e: React.KeyboardEvent<HTMLDivElement>) => void;
+};
+
+const Searchbar: FunctionComponent<SearchbarProps> = ({
+  handleSearch,
+  value,
+  onKeyDown,
+}) => {
   return (
     <div className="flex flex-row p-2 bg-background w-full rounded-lg">
       <Image src={SearchIcon} priority width={16} height={16} />
@@ -16,4 +26,6 @@ export default function Searchbar({ handleSearch, value, onKeyDown }) {
       />
     </div>
   );
-}
+};
+
+export default Searchbar;
