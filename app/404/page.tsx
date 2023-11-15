d00eb0844a304ab67432ad983d2946dec7934fde
Iris <iris.devillars@gmail.com>
2023-11-15 14:28:47 +0100

    fix: path

--- app/404/page.tsx
+++ app/404/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import type { NextPage } from "next";
-import styles from "../../../styles/Home.module.css";
+import styles from "../../styles/Home.module.css";
 
 const NotFound: NextPage = () => {
   return (
