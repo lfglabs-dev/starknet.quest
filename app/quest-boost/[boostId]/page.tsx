9ab25015f2d477ac579b24100b176b0f77db3ad1
ayushtom <tomar2585@gmail.com>
2023-11-30 12:48:54 +0530

    feat: add contract call base

--- app/quest-boost/[boostId]/page.tsx
+++ app/quest-boost/[boostId]/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 
-import React from "react";
+import React, { useContext, useMemo } from "react";
 import styles from "../../../styles/questboost.module.css";
 import BoostCard from "../../../components/quest-boost/boostCard";
+import { Abi, Contract, shortString } from "starknet";
+import { StarknetIdJsContext } from "../../../context/StarknetIdJsProvider";
 
 type BoostQuestPageProps = {
   params: {
@@ -12,6 +14,38 @@ type BoostQuestPageProps = {
 
 export default function Page({ params }: BoostQuestPageProps) {
   const { boostId } = params;
+  const { starknetIdNavigator } = useContext(StarknetIdJsContext);
+
+  // const contract = useMemo(() => {
+  //   return new Contract(
+  //     naming_abi as Abi,
+  //     process.env.NEXT_PUBLIC_NAMING_CONTRACT as string,
+  //     starknetIdNavigator?.provider
+  //   );
+  // }, [starknetIdNavigator?.provider]);
+
+  // const callContract = async (
+  //   contract: Contract,
+  //   token: string,
+  //   amount: string,
+  //   signature: string[]
+  // ): Promise<void> => {
+  //   try {
+  //     const res = await contract.call("claim", [
+  //       amount,
+  //       token,
+  //       signature,
+  //       boostId,
+  //     ]);
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // };
+
+  // const handleClaimClick = async () => {
+  //   const contractAddress = "";
+  // };
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Name of boost</h1>
