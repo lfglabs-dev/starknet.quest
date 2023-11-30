74abee6427a069d8c39c99671022a5064bb80062
Thomas Marchand <thomas.marchand@tuta.io>
2023-11-30 13:52:46 +0000

    fix: remove useless url rewrites

--- next.config.js
+++ next.config.js
@@ -1,34 +1,6 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-  rewrites() {
-    return {
-      beforeFiles: [
-        // if the host is `app.acme.com`,
-        // this rewrite will be applied
-        {
-          source: "/:path*",
-          has: [
-            {
-              type: "host",
-              value: "indexer.starknet.id",
-            },
-          ],
-          destination: "/api/indexer/:path*",
-        },
-        {
-          source: "/:path*",
-          has: [
-            {
-              type: "host",
-              value: "goerli.indexer.starknet.id",
-            },
-          ],
-          destination: "/api/indexer/:path*",
-        },
-      ],
-    };
-  },
   reactStrictMode: true,
   webpack: (config, { isServer }) => {
     if (!isServer) {
