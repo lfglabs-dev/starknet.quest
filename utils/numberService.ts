603673607594c08b3e99353032444451410784ed
ayushtom <tomar2585@gmail.com>
2023-12-27 22:44:35 +0530

    fix: utils

--- utils/numberService.ts
+++ utils/numberService.ts
@@ -1,5 +1,5 @@
 export const calculatePercentile = (rank: number, total: number): number => {
-  if (total === rank) return 100;
+  if (total === rank) return 0;
   if (rank === 1) return 100;
   return parseInt((((total - rank) / total) * 100).toFixed(2));
 };
