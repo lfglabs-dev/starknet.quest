7de8521fb46396b1fd9c02a799fbeecd96c507a3
ayushtom <tomar2585@gmail.com>
2024-01-09 01:30:05 +0530

    chore: make abs import

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useContext } from "react";
-import styles from "../styles/Home.module.css";
+import styles from "@styles/Home.module.css";
 
 import { useRouter } from "next/navigation";
 import HowToParticipate from "@components/pages/home/howToParticipate";
