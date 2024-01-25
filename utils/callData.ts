dd4560ad26c6f9721c66fd7cc451c63d65b0e811
ayushtom <tomar2585@gmail.com>
2024-01-26 00:39:05 +0530

    test

--- utils/callData.ts
+++ utils/callData.ts
@@ -3,7 +3,7 @@ import { CallData, uint256 } from "starknet";
 export function boostClaimCall(boost: Boost, sign: Signature) {
   const amount = uint256.bnToUint256(
     parseInt(
-      parseInt(String(boost.amount / boost.num_of_winners) * Math.pow(10, 6))
+      parseInt(String(boost.amount / boost.num_of_winners)) * Math.pow(10, 6)
     )
   );
   console.log("amount", amount);
