9ac63b56e71956701d732022e5ab658aa92687de
Iris <iris.devillars@gmail.com>
2024-02-21 12:25:55 +0100

    fix: notification manager (#511)

--- hooks/useNotificationManager.ts
+++ hooks/useNotificationManager.ts
@@ -1,9 +1,7 @@
-import { useAccount, useProvider } from "@starknet-react/core";
+import { useAccount } from "@starknet-react/core";
 import { useAtom } from "jotai";
 import { atomWithStorage } from "jotai/utils";
-import { useEffect } from "react";
 import { hexToDecimal } from "@utils/feltService";
-import { NotificationType } from "@constants/notifications";
 
 const notificationsAtom = atomWithStorage<SQNotification<NotificationData>[]>(
   "userNotifications_SQ",
@@ -15,48 +13,19 @@ const readStatusAtom = atomWithStorage<boolean>(
 );
 
 export function useNotificationManager() {
-  const { provider } = useProvider();
   const { address } = useAccount();
   const [notifications, setNotifications] = useAtom(notificationsAtom);
   const [unreadNotifications, setUnread] = useAtom(readStatusAtom);
 
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      notifications.forEach(checkTransactionStatus);
-    }, 5000);
-
-    return () => clearInterval(intervalId); // Cleanup on component unmount
-  }, [notifications]);
-
-  const checkTransactionStatus = async (
-    notification: SQNotification<NotificationData>,
-    index: number
+  const updateNotificationStatus = (
+    txHash: string,
+    status: "error" | "pending" | "success"
   ) => {
-    if (notification.type !== NotificationType.TRANSACTION) return;
-    if (notification.address !== hexToDecimal(address)) return;
-    if (notification.data.status === "pending") {
-      const transaction = notification.data;
-      const data = await provider.getTransactionReceipt(transaction.hash);
-      const updatedTransactions = [...notifications];
-
-      if (data?.status === "REJECTED" || data?.status === "REVERTED") {
-        updatedTransactions[index].data.status = "error";
-        updatedTransactions[index].data.txStatus = "REJECTED";
-        setNotifications(updatedTransactions);
-        setUnread(true);
-      } else if (
-        data?.status === "ACCEPTED_ON_L2" ||
-        data?.status === "ACCEPTED_ON_L1" ||
-        data?.finality_status === "ACCEPTED_ON_L2" ||
-        data?.finality_status === "ACCEPTED_ON_L1"
-      ) {
-        updatedTransactions[index].data.txStatus =
-          data?.status ?? data?.finality_status;
-        updatedTransactions[index].data.status = "success";
-        setNotifications(updatedTransactions);
-        setUnread(true);
-      }
-    }
+    const index = notifications.findIndex(
+      (notification) => notification.data.hash === txHash
+    );
+    notifications[index].data.status = status;
+    setNotifications(notifications);
   };
 
   const filteredNotifications = address
@@ -84,5 +53,6 @@ export function useNotificationManager() {
     addTransaction,
     unreadNotifications,
     updateReadStatus,
+    updateNotificationStatus,
   };
 }
