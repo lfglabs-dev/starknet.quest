24edc65711968d3fd3c23d57406d7d7f0a0c8d69
ayushtom <tomar2585@gmail.com>
2024-03-05 17:59:08 +0530

    test commit

--- app/analytics/[questId]/page.tsx
+++ app/analytics/[questId]/page.tsx
@@ -71,6 +71,8 @@ export default function Page({ params }: BoostQuestPageProps) {
   const { address } = useAccount();
   const { boostId } = params;
 
+  //get data from the server
+
   return (
     <div className={styles.container}>
       <div className={styles.backButton}>
