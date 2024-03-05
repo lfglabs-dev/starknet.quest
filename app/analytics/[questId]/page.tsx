e88e2fb068280f638573656106bd3c00431d3bad
ayushtom <tomar2585@gmail.com>
2024-03-05 17:36:27 +0530

    park changes

---
+++ app/analytics/[questId]/page.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import React from "react";
+import styles from "@styles/questboost.module.css";
+import { useRouter } from "next/navigation";
+import { useAccount } from "@starknet-react/core";
+import BackButton from "@components/UI/backButton";
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+
+type BoostQuestPageProps = {
+  params: {
+    boostId: string;
+  };
+};
+
+const data = [
+  {
+    name: "Page A",
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: "Page B",
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: "Page C",
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: "Page D",
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: "Page E",
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: "Page F",
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: "Page G",
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
+
+export default function Page({ params }: BoostQuestPageProps) {
+  const router = useRouter();
+  const { address } = useAccount();
+  const { boostId } = params;
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.backButton}>
+        <BackButton onClick={() => router.back()} />
+      </div>
+      <ResponsiveContainer width="100%" height={400}>
+        <AreaChart
+          width={500}
+          height={400}
+          data={data}
+          margin={{
+            top: 10,
+            right: 30,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
+        </AreaChart>
+      </ResponsiveContainer>
+    </div>
+  );
+}
