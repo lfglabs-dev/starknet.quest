d91523f1be6b2799ea431540d7195875a8115cd6
BlackStarkGoku <165695008+BlackStarkGoku@users.noreply.github.com>
2024-04-04 16:31:55 +0200

    update: make text centered for Quest cards on mobile

--- components/quests/quest.tsx
+++ components/quests/quest.tsx
@@ -47,7 +47,7 @@ const Quest: FunctionComponent<QuestProps> = ({
       >
         <p className="text-gray-400">{issuer.name}</p>
       </div>
-      <div className="flex gap-2 mt-3">
+      <div className="flex gap-2 mt-3 justify-center md:justify-start">
         <div className={styles.issuer}>
           {isCompleted ? (
             <>
