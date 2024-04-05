4e7ff9c36c2d4f2c091d40700b8933e138c33202
Catherine Jonathan <35270183+kateberryd@users.noreply.github.com>
2024-04-05 14:51:46 +0100

    fix: fixed quest status display when wallet is not connected

--- components/quest-boost/boostCard.tsx
+++ components/quest-boost/boostCard.tsx
@@ -12,120 +12,124 @@ import theme from "@styles/theme";
 import { useAccount } from "@starknet-react/core";
 
 type BoostCardProps = {
-  boost: Boost;
-  completedQuests?: number[];
+	boost: Boost;
+	completedQuests?: number[];
 };
 
 const BoostCard: FunctionComponent<BoostCardProps> = ({
-  boost,
-  completedQuests,
+	boost,
+	completedQuests,
 }) => {
-  const { address } = useAccount();
-  const [userParticipationStatus, setUserParticipationStatus] =
-    useState<boolean>(false);
-  const [userBoostCheckStatus, setUserBoostCheckStatus] =
-    useState<boolean>(false);
-  const [hasUserCompletedBoost, setHasUserCompletedBoost] =
-    useState<boolean>(false);
-  const { getBoostClaimStatus } = useBoost();
-  const [hovered, setHovered] = useState<boolean>(false);
+	const { address } = useAccount();
+	const [userParticipationStatus, setUserParticipationStatus] =
+		useState<boolean>(false);
+	const [userBoostCheckStatus, setUserBoostCheckStatus] =
+		useState<boolean>(false);
+	const [hasUserCompletedBoost, setHasUserCompletedBoost] =
+		useState<boolean>(false);
+	const { getBoostClaimStatus } = useBoost();
+	const [hovered, setHovered] = useState<boolean>(false);
 
-  useEffect(() => {
-    if (!boost || !completedQuests) return;
-    let userParticipationCheck = false;
-    let userBoostCompletionCheck = true;
-    boost.quests.forEach((quest) => {
-      // no quests are completed by user
-      if (!completedQuests) return false;
-      // if any of the quests are completed by user and is part of this boost
-      if (completedQuests.includes(quest)) userParticipationCheck = true;
-      // check if all quests are completed by the user and if not then set this flag value to false
-      if (!completedQuests.includes(quest)) userBoostCompletionCheck = false;
-    });
-    setHasUserCompletedBoost(userBoostCompletionCheck);
-    setUserParticipationStatus(userParticipationCheck ? true : false);
-  }, [completedQuests, boost]);
+	useEffect(() => {
+		if (!boost || !completedQuests) return;
+		let userParticipationCheck = false;
+		let userBoostCompletionCheck = true;
+		boost.quests.forEach((quest) => {
+			// no quests are completed by user
+			if (!completedQuests) return false;
+			// if any of the quests are completed by user and is part of this boost
+			if (completedQuests.includes(quest)) userParticipationCheck = true;
+			// check if all quests are completed by the user and if not then set this flag value to false
+			if (!completedQuests.includes(quest)) userBoostCompletionCheck = false;
+		});
+		setHasUserCompletedBoost(userBoostCompletionCheck);
+		setUserParticipationStatus(userParticipationCheck ? true : false);
+	}, [completedQuests, boost]);
 
-  useEffect(() => {
-    if (!address) return;
-    const res = getBoostClaimStatus(address, boost?.id);
-    setUserBoostCheckStatus(res);
-  }, [address]);
+	useEffect(() => {
+		if (!address) return;
+		const res = getBoostClaimStatus(address, boost?.id);
+		setUserBoostCheckStatus(res);
+	}, [address]);
 
-  const isClickable = useMemo(
-    () => !userBoostCheckStatus && address && hasUserCompletedBoost,
+	const isClickable = useMemo(
+		() => !userBoostCheckStatus && address && hasUserCompletedBoost,
 
-    [userBoostCheckStatus, address, hasUserCompletedBoost]
-  );
-  return (
-    <Link
-      className="flex"
-      href={isClickable ? `/quest-boost/${boost?.id}` : ""}
-    >
-      <div
-        className={styles.boost_card_container}
-        style={{
-          borderColor:
-            hovered && isClickable
-              ? theme.palette.secondary.dark
-              : "transparent",
-          cursor: isClickable ? "inherit" : "not-allowed",
-        }}
-        onMouseOver={() => setHovered(true)}
-        onMouseLeave={() => setHovered(false)}
-      >
-        <img
-          className={Cardstyles.cardImage}
-          src={boost?.img_url}
-          alt="boost"
-        />
-        <div className={styles.boost_card_content}>
-          <p className={styles.card_title}>{boost?.name}</p>
-          <p>
-            {boost?.quests.length} quest{boost.quests.length > 1 ? "s" : ""}
-          </p>
-          {!hasUserCompletedBoost && boost.expiry > Date.now() ? (
-            <>
-              <div className={questStyles.issuer}>
-                <p>{boost?.amount}</p>
-                <TokenSymbol tokenAddress={boost.token} />
-              </div>
-            </>
-          ) : (
-            <div className="flex w-full">
-              <div className="flex items-center">
-                <div className={styles.issuer}>
-                  {boost.expiry > Date.now() ? (
-                    hasUserCompletedBoost ? (
-                      <>
-                        <p className="text-white capitalize">Done</p>
-                        <CheckIcon width="24" color="#6AFFAF" />
-                      </>
-                    ) : null
-                  ) : boost.winner === null ? (
-                    <>
-                      <p className="text-white capitalize">Done</p>
-                      <CheckIcon width="24" color="#6AFFAF" />
-                    </>
-                  ) : isClickable ? (
-                    <>
-                      <p className="text-white capitalize">See my reward</p>
-                      <TrophyIcon width="24" color="#8BEED9" />
-                    </>
-                  ) : (
-                    <>
-                      <UnavailableIcon width="24" color="#D32F2F" />
-                      <p className="text-white capitalize mr-2">Boost ended</p>
-                    </>
-                  )}
-                </div>
-              </div>
-            </div>
-          )}
-        </div>
-      </div>
-    </Link>
-  );
+		[userBoostCheckStatus, address, hasUserCompletedBoost]
+	);
+	return (
+		<Link
+			className="flex"
+			href={isClickable ? `/quest-boost/${boost?.id}` : ""}
+		>
+			<div
+				className={styles.boost_card_container}
+				style={{
+					borderColor:
+						hovered && isClickable
+							? theme.palette.secondary.dark
+							: "transparent",
+					cursor: isClickable ? "inherit" : "not-allowed",
+				}}
+				onMouseOver={() => setHovered(true)}
+				onMouseLeave={() => setHovered(false)}
+			>
+				<img
+					className={Cardstyles.cardImage}
+					src={boost?.img_url}
+					alt="boost"
+				/>
+				<div className={styles.boost_card_content}>
+					<p className={styles.card_title}>{boost?.name}</p>
+					<p>
+						{boost?.quests.length} quest{boost.quests.length > 1 ? "s" : ""}
+					</p>
+					{!hasUserCompletedBoost && boost.expiry > Date.now() ? (
+						<>
+							<div className={questStyles.issuer}>
+								<p>{boost?.amount}</p>
+								<TokenSymbol tokenAddress={boost.token} />
+							</div>
+						</>
+					) : (
+						<div className="flex w-full">
+							<div className="flex items-center">
+								{address && (
+									<div className={styles.issuer}>
+										{boost.expiry > Date.now() ? (
+											hasUserCompletedBoost ? (
+												<>
+													<p className="text-white capitalize">Done</p>
+													<CheckIcon width="24" color="#6AFFAF" />
+												</>
+											) : null
+										) : boost.winner === null ? (
+											<>
+												<p className="text-white capitalize">Done</p>
+												<CheckIcon width="24" color="#6AFFAF" />
+											</>
+										) : isClickable ? (
+											<>
+												<p className="text-white capitalize">See my reward</p>
+												<TrophyIcon width="24" color="#8BEED9" />
+											</>
+										) : (
+											<>
+												<UnavailableIcon width="24" color="#D32F2F" />
+												<p className="text-white capitalize mr-2">
+													Boost ended
+												</p>
+											</>
+										)}
+									</div>
+								)}
+							</div>
+						</div>
+					)}
+				</div>
+			</div>
+		</Link>
+	);
 };
 
 export default BoostCard;
