17cbcda949115da3b424d3c8515a2e3eb3ad2242
Catherine Jonathan <35270183+kateberryd@users.noreply.github.com>
2024-04-11 07:38:37 +0100

    fix: add check to updateBoostClaim status

--- components/quest-boost/boostCard.tsx
+++ components/quest-boost/boostCard.tsx
@@ -13,157 +13,157 @@ import { useAccount } from "@starknet-react/core";
 import { useRouter } from "next/navigation";
 
 type BoostCardProps = {
-	boost: Boost;
-	completedQuests?: number[];
+  boost: Boost;
+  completedQuests?: number[];
 };
 
 const BoostCard: FunctionComponent<BoostCardProps> = ({
-	boost,
-	completedQuests,
+  boost,
+  completedQuests,
 }) => {
-	const { address } = useAccount();
-	const [userParticipationStatus, setUserParticipationStatus] =
-		useState<boolean>(false);
-	const [userBoostCheckStatus, setUserBoostCheckStatus] =
-		useState<boolean>(false);
-	const [hasUserCompletedBoost, setHasUserCompletedBoost] =
-		useState<boolean>(false);
-	const { getBoostClaimStatus, updateBoostClaimStatus } = useBoost();
-	const [hovered, setHovered] = useState<boolean>(false);
-	const navigate = useRouter();
+  const { address } = useAccount();
+  const [userParticipationStatus, setUserParticipationStatus] =
+    useState<boolean>(false);
+  const [userBoostCheckStatus, setUserBoostCheckStatus] =
+    useState<boolean>(false);
+  const [hasUserCompletedBoost, setHasUserCompletedBoost] =
+    useState<boolean>(false);
+  const { getBoostClaimStatus, updateBoostClaimStatus } = useBoost();
+  const [hovered, setHovered] = useState<boolean>(false);
+  const navigate = useRouter();
 
-	useEffect(() => {
-		if (!boost || !completedQuests) return;
-		let userParticipationCheck = false;
-		let userBoostCompletionCheck = true;
-		boost.quests.forEach((quest) => {
-			// no quests are completed by user
-			if (!completedQuests) return false;
-			// if any of the quests are completed by user and is part of this boost
-			if (completedQuests.includes(quest)) userParticipationCheck = true;
+  useEffect(() => {
+    if (!boost || !completedQuests) return;
+    let userParticipationCheck = false;
+    let userBoostCompletionCheck = true;
+    boost.quests.forEach((quest) => {
+      // no quests are completed by user
+      if (!completedQuests) return false;
+      // if any of the quests are completed by user and is part of this boost
+      if (completedQuests.includes(quest)) userParticipationCheck = true;
 
-			// check if all quests are completed by the user and if not then set this flag value to false
-			if (!completedQuests.includes(quest)) userBoostCompletionCheck = false;
-		});
-		setHasUserCompletedBoost(userBoostCompletionCheck);
-		setUserParticipationStatus(userParticipationCheck ? true : false);
-	}, [completedQuests, boost]);
+      // check if all quests are completed by the user and if not then set this flag value to false
+      if (!completedQuests.includes(quest)) userBoostCompletionCheck = false;
+    });
+    setHasUserCompletedBoost(userBoostCompletionCheck);
+    setUserParticipationStatus(userParticipationCheck ? true : false);
+  }, [completedQuests, boost]);
 
-	useEffect(() => {
-		if (!address) return;
-		const res = getBoostClaimStatus(address, boost?.id);
-		setUserBoostCheckStatus(res);
-	}, [address]);
+  useEffect(() => {
+    if (!address) return;
+    const res = getBoostClaimStatus(address, boost?.id);
+    setUserBoostCheckStatus(res);
+  }, [address]);
 
-	const isClickable = useMemo(() => {
-		if (
-			boost.expiry < Date.now() &&
-			hasUserCompletedBoost &&
-			!userBoostCheckStatus &&
-			boost.winner != null
-		) {
-			// see my reward check
-			return true;
-		} else if (boost.expiry > Date.now() && !hasUserCompletedBoost) {
-			// not expired and not completed by user
-			return true;
-		} else if (boost.expiry > Date.now() && hasUserCompletedBoost) {
-			// not expired and completed  by user
-			return true;
-		}
-		return false;
-	}, [userBoostCheckStatus, hasUserCompletedBoost]);
+  const isClickable = useMemo(() => {
+    if (
+      boost.expiry < Date.now() &&
+      hasUserCompletedBoost &&
+      !userBoostCheckStatus &&
+      boost.winner != null
+    ) {
+      // "see my reward" check
+      return true;
+    } else if (boost.expiry > Date.now() && !hasUserCompletedBoost) {
+      // not expired and not completed by user
+      return true;
+    } else if (boost.expiry > Date.now() && hasUserCompletedBoost) {
+      // not expired and completed  by user
+      return true;
+    }
+    return false;
+  }, [userBoostCheckStatus, hasUserCompletedBoost]);
 
-	const navigateAndUpdateBoostStatus = () => {
-		if (isClickable) {
-			navigate.push(isClickable ? `/quest-boost/${boost?.id}` : "");
-		}
-		if (hasUserCompletedBoost && !userBoostCheckStatus) {
-			updateBoostClaimStatus(address ?? "", boost.id, !userBoostCheckStatus);
-		}
-	};
+  const navigateAndUpdateBoostStatus = () => {
+    if (isClickable) {
+      navigate.push(isClickable ? `/quest-boost/${boost?.id}` : "");
+    }
+    if (hasUserCompletedBoost && !userBoostCheckStatus) {
+      updateBoostClaimStatus(address ?? "", boost.id, !userBoostCheckStatus);
+    }
+  };
 
-	return (
-		<div className="flex" onClick={navigateAndUpdateBoostStatus}>
-			<div
-				className={styles.boost_card_container}
-				style={{
-					borderColor:
-						hovered && isClickable
-							? theme.palette.secondary.dark
-							: "transparent",
-					cursor: isClickable ? "inherit" : "not-allowed",
-				}}
-				onMouseOver={() => setHovered(true)}
-				onMouseLeave={() => setHovered(false)}
-			>
-				<img
-					className={Cardstyles.cardImage}
-					src={boost?.img_url}
-					alt="boost"
-				/>
-				<div className={styles.boost_card_content}>
-					<p className={styles.card_title}>{boost?.name}</p>
-					<p>
-						{boost?.quests.length} quest{boost.quests.length > 1 ? "s" : ""}
-					</p>
-					{!hasUserCompletedBoost && boost.expiry > Date.now() ? (
-						<>
-							<div className={questStyles.issuer}>
-								<p>{boost?.amount}</p>
-								<TokenSymbol tokenAddress={boost.token} />
-							</div>
-						</>
-					) : (
-						<div className="flex w-full">
-							<div className="flex items-center">
-								{address && (
-									<div className={styles.issuer}>
-										{boost.expiry < Date.now() ? (
-											hasUserCompletedBoost ? (
-												!userBoostCheckStatus ? (
-													boost.winner != null ? (
-														<>
-															<p className="text-white">See my rewards</p>
-															<CheckIcon width="24" color="#6AFFAF" />
-														</>
-													) : (
-														<>
-															<UnavailableIcon width="24" color="#D32F2F" />
-															<p className="text-white mr-2">Boost ended</p>
-														</>
-													)
-												) : (
-													userBoostCheckStatus && (
-														<>
-															<UnavailableIcon width="24" color="#D32F2F" />
-															<p className="text-white mr-2">Boost ended</p>
-														</>
-													)
-												)
-											) : (
-												<>
-													<UnavailableIcon width="24" color="#D32F2F" />
-													<p className="text-white mr-2">Boost ended</p>
-												</>
-											)
-										) : (
-											hasUserCompletedBoost && (
-												<>
-													<p className="text-white">Done</p>
-													<CheckIcon width="24" color="#6AFFAF" />
-												</>
-											)
-										)}
-									</div>
-								)}
-							</div>
-						</div>
-					)}
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex" onClick={navigateAndUpdateBoostStatus}>
+      <div
+        className={styles.boost_card_container}
+        style={{
+          borderColor:
+            hovered && isClickable
+              ? theme.palette.secondary.dark
+              : "transparent",
+          cursor: isClickable ? "inherit" : "not-allowed",
+        }}
+        onMouseOver={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
+      >
+        <img
+          className={Cardstyles.cardImage}
+          src={boost?.img_url}
+          alt="boost"
+        />
+        <div className={styles.boost_card_content}>
+          <p className={styles.card_title}>{boost?.name}</p>
+          <p>
+            {boost?.quests.length} quest{boost.quests.length > 1 ? "s" : ""}
+          </p>
+          {!hasUserCompletedBoost && boost.expiry > Date.now() ? (
+            <>
+              <div className={questStyles.issuer}>
+                <p>{boost?.amount}</p>
+                <TokenSymbol tokenAddress={boost.token} />
+              </div>
+            </>
+          ) : (
+            <div className="flex w-full">
+              <div className="flex items-center">
+                {address && (
+                  <div className={styles.issuer}>
+                    {boost.expiry < Date.now() ? (
+                      hasUserCompletedBoost ? (
+                        !userBoostCheckStatus ? (
+                          boost.winner != null ? (
+                            <>
+                              <p className="text-white">See my rewards</p>
+                              <CheckIcon width="24" color="#6AFFAF" />
+                            </>
+                          ) : (
+                            <>
+                              <UnavailableIcon width="24" color="#D32F2F" />
+                              <p className="text-white mr-2">Boost ended</p>
+                            </>
+                          )
+                        ) : (
+                          userBoostCheckStatus && (
+                            <>
+                              <UnavailableIcon width="24" color="#D32F2F" />
+                              <p className="text-white mr-2">Boost ended</p>
+                            </>
+                          )
+                        )
+                      ) : (
+                        <>
+                          <UnavailableIcon width="24" color="#D32F2F" />
+                          <p className="text-white mr-2">Boost ended</p>
+                        </>
+                      )
+                    ) : (
+                      hasUserCompletedBoost && (
+                        <>
+                          <p className="text-white">Done</p>
+                          <CheckIcon width="24" color="#6AFFAF" />
+                        </>
+                      )
+                    )}
+                  </div>
+                )}
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BoostCard;
