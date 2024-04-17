2d80bdec882dfcf1a8ee5995d547b46ad2cafe28
ayushtom <tomar2585@gmail.com>
2024-04-18 00:44:47 +0530

    test: add logs

--- utils/cdnize.ts
+++ utils/cdnize.ts
@@ -1,5 +1,11 @@
 // hard to test because of ENV variables
 export default function cdnize(src: string): string {
+  console.log({
+    test: process.env.VERCEL_ENV === "production" && src.startsWith("/"),
+    test2: process.env.NEXT_PUBLIC_CDN_URL,
+    test3: src,
+    test4: src.startsWith("/"),
+  });
   return process.env.VERCEL_ENV === "production" && src.startsWith("/")
     ? `${process.env.NEXT_PUBLIC_CDN_URL}${src}`
     : src;
