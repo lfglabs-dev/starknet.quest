b5d6fc0d36338248d3fc9bc41da2906f437d995f
ayushtom <tomar2585@gmail.com>
2024-04-18 00:48:56 +0530

    chore: add more logs

--- utils/cdnize.ts
+++ utils/cdnize.ts
@@ -5,6 +5,9 @@ export default function cdnize(src: string): string {
     test2: process.env.NEXT_PUBLIC_CDN_URL,
     test3: src,
     test4: src.startsWith("/"),
+    test5: `${process.env.NEXT_PUBLIC_CDN_URL}${src}`,
+    test6: process.env.VERCEL_ENV,
+    test7: process.env.NODE_ENV,
   });
   return process.env.VERCEL_ENV === "production" && src.startsWith("/")
     ? `${process.env.NEXT_PUBLIC_CDN_URL}${src}`
