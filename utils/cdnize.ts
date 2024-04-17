d10c3156d91a69d12a6a96879ea37539abfac090
ayushtom <tomar2585@gmail.com>
2024-04-18 00:52:15 +0530

    fix: cdn check

--- utils/cdnize.ts
+++ utils/cdnize.ts
@@ -1,15 +1,6 @@
 // hard to test because of ENV variables
 export default function cdnize(src: string): string {
-  console.log({
-    test: process.env.VERCEL_ENV === "production" && src.startsWith("/"),
-    test2: process.env.NEXT_PUBLIC_CDN_URL,
-    test3: src,
-    test4: src.startsWith("/"),
-    test5: `${process.env.NEXT_PUBLIC_CDN_URL}${src}`,
-    test6: process.env.VERCEL_ENV,
-    test7: process.env.NODE_ENV,
-  });
-  return process.env.VERCEL_ENV === "production" && src.startsWith("/")
+  return process.env.NODE_ENV === "production" && src.startsWith("/")
     ? `${process.env.NEXT_PUBLIC_CDN_URL}${src}`
     : src;
 }
