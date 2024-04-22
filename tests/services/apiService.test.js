8960ccb452662d23345ff335c2efe9af1431a9e4
Abdulhakeem Abdulazeez <ayoazeez26@gmail.com>
2024-04-22 10:42:44 +0100

    feat: added test for getBoostById api call

--- tests/services/apiService.test.js
+++ tests/services/apiService.test.js
@@ -1,4 +1,4 @@
-import { fetchQuestCategoryData } from "@services/apiService";
+import { fetchQuestCategoryData, getBoostById } from "@services/apiService";
 
 const API_URL = process.env.NEXT_PUBLIC_API_LINK;
 
@@ -39,4 +39,45 @@ describe("fetchQuestCategoryData function", () => {
     );
     expect(result).toEqual(mockResponse);
   });
+});
+
+describe("getBoostById function", () => {
+  beforeEach(() => {
+    fetch.mockClear();
+  });
+
+  it("should fetch and return data for a valid boost id", async () => {
+    const mockData = {
+      amount: 1000,
+      expiry: 1718052414000,
+      hidden: false,
+      id: 9,
+      img_url: "/nostra/cigar.webp",
+      name: "nostra - Stake and Win",
+      num_of_winners: 4,
+      quests: [27],
+      token:
+        "0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d",
+      token_decimals: 18,
+      winner: null
+    };
+    fetch.mockResolvedValueOnce({
+      json: () => Promise.resolve(mockData),
+    });
+
+    const result = await getBoostById("BoostId");
+    expect(fetch).toHaveBeenCalledWith(`${API_URL}/boost/get_boost?id=BoostId`);
+    expect(result).toEqual(mockData);
+  })
+  
+  it("should handle fetcherrors gracefully", async () => {
+    const mockResponse = "Boost not found";
+    fetch.mockResolvedValueOnce({
+      json: () => Promise.resolve(mockResponse),
+    });
+
+    const result = await getBoostById("InvalidId");
+    expect(fetch).toHaveBeenCalledWith(`${API_URL}/boost/get_boost?id=InvalidId`);
+    expect(result).toEqual(mockResponse);
+  })
 });
