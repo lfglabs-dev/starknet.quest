1d1ec2945cbb4788544b289a39277e36ee3a39a0
Seun Jeremiah <seunjeremiah2003@gmail.com>
2024-04-24 12:56:25 +0100

    test: update unique visitors

--- tests/services/apiService.test.js
+++ tests/services/apiService.test.js
@@ -1,4 +1,4 @@
-import { fetchQuestCategoryData } from "@services/apiService";
+import { fetchQuestCategoryData, updateUniqueVisitors } from "@services/apiService";
 
 const API_URL = process.env.NEXT_PUBLIC_API_LINK;
 
@@ -36,6 +36,41 @@ describe("fetchQuestCategoryData function", () => {
     const result = await fetchQuestCategoryData("InvalidCategory");
     expect(fetch).toHaveBeenCalledWith(
       `${API_URL}/get_quest_category?name=InvalidCategory`
+    );
+    expect(result).toEqual(mockResponse);
+  });
+});
+
+
+describe("updateUniqueVisitors function", () => {
+  beforeEach(() => {
+    fetch.mockClear();
+  });
+
+  it("should fetch unique visitor count", async () => {
+    const mockData = {
+      res: "unique visitor count",
+    };
+    fetch.mockResolvedValueOnce({
+      json: () => Promise.resolve(mockData),
+    });
+
+    const result = await updateUniqueVisitors("1");
+    expect(fetch).toHaveBeenCalledWith(
+      `${API_URL}/unique_page_visit?page_id=${"1"}`
+    );
+    expect(result).toEqual(mockData);
+  });
+
+  it("should handle fetch errors gracefully", async () => {
+    const mockResponse = "Category not found";
+    fetch.mockResolvedValueOnce({
+      json: () => Promise.resolve(mockResponse),
+    });
+
+    const result = await updateUniqueVisitors("3");
+    expect(fetch).toHaveBeenCalledWith(
+      `${API_URL}/unique_page_visit?page_id=${"3"}`
     );
     expect(result).toEqual(mockResponse);
   });
