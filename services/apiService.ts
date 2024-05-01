4b982ae1a47c752a515bec159c647e4283f539fc
Iwueseiter <preciouschia19@gmail.com>
2024-05-01 13:15:03 +0100

    refac(getQuests): modify getQuests() return type

--- services/apiService.ts
+++ services/apiService.ts
@@ -121,7 +121,7 @@ export const getCompletedBoosts = async (addr: string) => {
   }
 };
 
-export const getQuests = async (): Promise<QuestDocument | QueryError> => {
+export const getQuests = async (): Promise<QuestDocument | QueryError | undefined> => {
   try {
     const response = await fetch(`${baseurl}/get_quests`);
     return await response.json();
