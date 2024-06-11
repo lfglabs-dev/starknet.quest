6932f8abc397e706f4d7be1ae04169fa91042e03
juandiegocv27 <juandiegocarballo2701@gmail.com>
2024-06-11 12:29:51 -0600

    feat: put the accentbox in statselemnt.tsx

--- components/UI/stats/statElement.tsx
+++ components/UI/stats/statElement.tsx
@@ -3,6 +3,7 @@ import styles from "@styles/components/stats.module.css";
 import Box from "../box";
 import Typography from "../typography/typography";
 import { TEXT_TYPE } from "@constants/typography";
+import AccentBox from "@components/UI/AccentBox";
 
 type StatElementProps = {
   name: string;
@@ -12,11 +13,14 @@ type StatElementProps = {
 const StatElement: FunctionComponent<StatElementProps> = ({ name, value }) => {
   return (
     <div className="flex flex-1 w-full px-4 sm:px-2">
+      <AccentBox>
       <Box>
         <Typography type={TEXT_TYPE.BODY_NORMAL} color="transparent" className={styles.statValue}>{value}</Typography>
         <Typography type={TEXT_TYPE.BODY_DEFAULT} color="secondary" className={styles.statName}>{name}</Typography>
       </Box>
+      </AccentBox>
     </div>
+    
   );
 };
 
