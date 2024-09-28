f77a295fe982e8e0d0e379bbe74ed5f31a8ed876
nicolasito1411 <60229704+Marchand-Nicolas@users.noreply.github.com>
2024-09-28 10:41:33 +0200

    test

--- hooks/naming.ts
+++ hooks/naming.ts
@@ -17,15 +17,22 @@ export function useDomainFromAddress(
 
   useEffect(() => {
     if (!address) return;
+    console.log("============================");
     const fetchStarkName = async () => {
       const domain = await starknetIdNavigator
         ?.getStarkName(address.toString())
         .catch((err) => {
+          console.log("err", err);
           setError(err);
         });
+      console.error("aaa", domain);
       setDomain(domain as string);
     };
     fetchStarkName();
+    const interval = setInterval(() => {
+      fetchStarkName();
+    }, 1000);
+    return () => clearInterval(interval);
   }, [starknetIdNavigator, address]);
 
   return { domain, error };
