0691692c6fb8ba65cff2eee8497985038ebd1712
lauchaves <lauchavesmendez@gmail.comA>
2024-10-05 23:40:01 -0600

    merge-clickable-social-media-icon-component

---
+++ components/UI/actions/clickable/clickableSocialIcon.tsx
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from 'react';
+import { Tooltip } from '@mui/material';
+import TwitterIcon from '@components/UI/iconsComponents/icons/twitterIcon';
+import GithubIcon from '@components/UI/iconsComponents/icons/githubIcon';
+import DiscordIcon from '@components/UI/iconsComponents/icons/discordIcon';
+import VerifiedIcon from '@components/UI/iconsComponents/icons/verifiedIcon';
+import styles from '@styles/components/icons.module.css';
+import theme from '@styles/theme';
+
+type SocialPlatform = 'twitter' | 'github' | 'discord';
+
+interface ClickableSocialIconProps {
+  platform: SocialPlatform;
+  width: string;
+  profileId: string;
+  domain?: string;
+}
+
+const ClickableSocialIcon: React.FC<ClickableSocialIconProps> = ({
+  platform,
+  width,
+  profileId,
+  domain,
+}) => {
+  const [githubUsername, setGithubUsername] = useState<string | undefined>();
+
+  useEffect(() => {
+    if (platform === 'github' && profileId) {
+      fetch(`https://api.github.com/user/${profileId}`)
+        .then(response => response.json())
+        .then(data => {
+          setGithubUsername(data.login);
+        });
+    }
+  }, [platform, profileId]);
+
+  const getIcon = () => {
+    switch (platform) {
+      case 'twitter':
+        return <TwitterIcon width={width} color="#1DA1F2" />;
+      case 'github':
+        return <GithubIcon width={width} color="#101012" />;
+      case 'discord':
+        return <DiscordIcon width={width} color="#5865F2" />;
+      default:
+        return null;
+    }
+  };
+
+  const getUrl = () => {
+    switch (platform) {
+      case 'twitter':
+        return `https://twitter.com/${profileId}`;
+      case 'github':
+        return githubUsername ? `https://github.com/${githubUsername}` : "#"	;
+      case 'discord':
+        return `https://discord.com/users/${profileId}`;
+      default:
+        return "#";
+    }
+  };
+
+  return profileId ? (
+    <Tooltip title={`Check ${domain ? domain + ' ' : ''}${platform}`} arrow>
+      <div className={styles.clickableIcon} onClick={() => window.open(getUrl())}>
+        <div className={styles.verifiedIcon}>
+          <VerifiedIcon width="18" color={theme.palette.primary.main} />
+        </div>
+        {getIcon()}
+      </div>
+    </Tooltip>
+  ) : null;
+};
+
+export default ClickableSocialIcon;
--- components/UI/actions/socialmediaActions.tsx
+++ components/UI/actions/socialmediaActions.tsx
@@ -1,9 +1,9 @@
-import React, { FunctionComponent, useEffect, useState } from "react";
-import ClickableDiscordIcon from "./clickable/clickableDiscordIcon";
-import ClickableGithubIcon from "./clickable/clickableGithubIcon";
-import ClickableTwitterIcon from "./clickable/clickableTwitterIcon";
-import { isStarkRootDomain } from "starknetid.js/packages/core/dist/utils";
-import { cairo } from "starknet";
+import React, { FunctionComponent, useEffect, useState } from 'react';
+import ClickableSocialIcon from './clickable/clickableSocialIcon';
+import { isStarkRootDomain } from 'starknetid.js/packages/core/dist/utils';
+import { cairo } from 'starknet';
+
+type SocialPlatform = 'twitter' | 'discord' | 'github';
 
 type SocialMediaActionsProps = {
   identity: Identity;
@@ -12,51 +12,43 @@ type SocialMediaActionsProps = {
 const SocialMediaActions: FunctionComponent<SocialMediaActionsProps> = ({
   identity,
 }) => {
-  const [twitter, setTwitter] = useState<string | undefined>();
-  const [discord, setDiscord] = useState<string | undefined>();
-  const [github, setGithub] = useState<string | undefined>();
+  const [socialProfiles, setSocialProfiles] = useState<{
+    twitter?: string;
+    discord?: string;
+    github?: string;
+  }>({
+    twitter: undefined,
+    discord: undefined,
+    github: undefined,
+  });
 
   useEffect(() => {
     if (isStarkRootDomain(identity?.domain.domain)) {
-      identity?.verifier_data?.forEach((verifier) => {
-        if (cairo.felt(verifier.field) === cairo.felt("twitter")) {
-          setTwitter(verifier.data);
-        }
-        if (cairo.felt(verifier.field) === cairo.felt("discord")) {
-          setDiscord(verifier.data);
-        }
-        if (cairo.felt(verifier.field) === cairo.felt("github")) {
-          setGithub(verifier.data);
-        }
+      const newProfiles: { twitter?: string; discord?: string; github?: string } = {};
+      identity?.verifier_data?.forEach(verifier => {
+        const field = cairo.felt(verifier.field);
+        if (field === cairo.felt('twitter')) newProfiles.twitter = verifier.data;
+        if (field === cairo.felt('discord')) newProfiles.discord = verifier.data;
+        if (field === cairo.felt('github')) newProfiles.github = verifier.data;
       });
+      setSocialProfiles(newProfiles);
     }
   }, [identity]);
 
   return (
     <div className="flex flex-row justify-evenly items-center gap-3">
-      {twitter ? (
-        <ClickableTwitterIcon
-          width="16"
-          domain={identity?.domain.domain}
-          twitterId={twitter}
-        />
-      ) : null}
-
-      {discord ? (
-        <ClickableDiscordIcon
-          width="16"
-          domain={identity?.domain.domain}
-          discordId={discord}
-        />
-      ) : null}
-
-      {github ? (
-        <ClickableGithubIcon
-          width="16"
-          domain={identity?.domain.domain}
-          githubId={github}
-        />
-      ) : null}
+      {Object.entries(socialProfiles).map(([platform, profileId]) => {
+        const socialPlatform = platform as SocialPlatform;
+        return profileId ? (
+          <ClickableSocialIcon
+            key={platform}
+            platform={socialPlatform}
+            width="16"
+            profileId={profileId as string}
+            domain={identity?.domain.domain}
+          />
+        ) : null;
+      })}
     </div>
   );
 };
