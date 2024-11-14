71acc4d88c93df6c9e56e8afea18d6923fb14e3e
yunusabdul <yunusabdulmajidyunus38@gmail.com>
2024-11-14 12:02:40 +0100

    Add skeleton for portfolio value sum

--- components/UI/profileCard/profileCard.tsx
+++ components/UI/profileCard/profileCard.tsx
@@ -29,7 +29,6 @@ import Typography from "../typography/typography";
 import { calculateTotalBalance } from "../../../services/argentPortfolioService";
 import Avatar from "../avatar";
 import { useHidePortfolio } from "@hooks/useHidePortfolio";
-import Loading from '@components/skeletons/loading';
 
 const MAX_RETRIES = 1000;
 const RETRY_DELAY = 2000;
@@ -166,8 +165,16 @@ const ProfileCard: FunctionComponent<ProfileCardProps> = ({
           </Typography>
           <div className={styles.address_div}>
             <div className='flex items-center gap-2 h-6'>
-                  <Loading isLoading={totalBalance === null}>
-                   <Typography
+            {totalBalance === null?
+            <Skeleton
+            variant='text'
+            sx={{ bgcolor: "grey.600" }}
+            width={60}
+            height={30}
+          />
+            :
+            <>
+               <Typography
                 type={TEXT_TYPE.BODY_SMALL}
                 className={`${styles.wallet_amount} font-extrabold`}
               >
@@ -185,15 +192,14 @@ const ProfileCard: FunctionComponent<ProfileCardProps> = ({
                   />
                 )}
               </Typography>
-                  </Loading>
-                <Loading isLoading={totalBalance === null} loadingType='spinner'>
-                <div
+              <div
                   onClick={() => setHidePortfolio(!hidePortfolio)}
                   className='cursor-pointer'
                 >
                   {hidePortfolio ? <EyeIconSlashed /> : <EyeIcon />}
                 </div>
-                </Loading>
+            </>  
+          }   
             </div>
           </div>
           <div className='flex sm:hidden justify-center py-4'>
