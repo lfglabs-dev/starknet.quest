3770505b968eac2cd1aa4af0db4300b3caf8f1e8
Verifieddanny <dannyclassic56@gmail.com>
2025-03-09 15:23:16 +0100

    Feat: Add patern circle svg animation

--- app/discover/defi/page.tsx
+++ app/discover/defi/page.tsx
@@ -13,6 +13,7 @@ import React, { useEffect, useCallback } from "react";
 import Typography from "@components/UI/typography/typography";
 import { TEXT_TYPE } from "@constants/typography";
 import { DEFI_CONCEPTS } from "./constants";
+import Image from "next/image";
 
 export default function Page() {
   const [data, setData] = React.useState<TableInfo[]>([]);
@@ -50,13 +51,26 @@ export default function Page() {
         <DataTable loading={loading} data={data} />
       </div>
 
-      <div className="w-full lg:w-3/4 px-5 lg:px-0">
-        <div className="max-w-sm md:max-w-none mx-auto">
-          <Typography type={TEXT_TYPE.H2} className="mb-8 text-center lg:text-left text-white font-bold text-2xl">
+      <div className="w-full lg:w-3/4 px-4 sm:px-6 md:px-8 2xl:px-32 relative overflow-x-hidden no-scrollbar">
+        <div className="absolute left-1/2 top-0 translate-x-[5rem] md:translate-x-[8rem]  2xl:translate-x-[34rem] w-[781px] h-[764px] opacity-30 pointer-events-none">
+          <Image
+            src="/icons/patternCircle.svg"
+            alt="pattern-circle"
+            width={781}
+            height={764}
+            className="w-full h-full relative animate-spin-slow"
+          />
+        </div>
+
+        <div className="max-w-md sm:max-w-lg md:max-w-none mx-auto relative z-10">
+          <Typography
+            type={TEXT_TYPE.H2}
+            className="mb-6 sm:mb-8 text-center sm:text-left text-white font-bold text-xl sm:text-2xl"
+          >
             Essential DeFi Concepts
           </Typography>
           <div
-            className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
+            className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
             aria-label="DeFi concept cards grid"
           >
             {DEFI_CONCEPTS.map((concept) => (
