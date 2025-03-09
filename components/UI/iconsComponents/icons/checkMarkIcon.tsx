9e208be401fe5a27cdd22a75c08b7bb631532fbe
Abdulhakeem Abdulazeez <ayoazeez26@gmail.com>
2025-03-09 13:57:53 +0100

    feat: completed refont quiz final review

--- components/UI/iconsComponents/icons/checkMarkIcon.tsx
+++ components/UI/iconsComponents/icons/checkMarkIcon.tsx
@@ -9,13 +9,8 @@ const CheckMarkIcon: FunctionComponent<IconProps> = ({ width }) => {
       fill="none"
       viewBox="0 0 24 24"
       strokeWidth={1.5}
-      stroke="currentColor"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M4.5 12.75l6 6 9-13.5"
-      />
+      stroke="currentColor">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M20 6 9 17l-5-5" />
     </svg>
   );
 };
