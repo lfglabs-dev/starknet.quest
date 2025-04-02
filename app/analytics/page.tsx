c4e0911d3a6f1b5c5ad1eaf38aa7ba3c490e047c
Nico <60229704+Marchand-Nicolas@users.noreply.github.com>
2025-04-02 11:27:44 +0200

    remove duplicate

--- app/analytics/page.tsx
+++ app/analytics/page.tsx
@@ -56,13 +56,6 @@ export default function Page() {
       >
         Quest Analytics
       </Typography>
-      <Typography
-        type={TEXT_TYPE.H1}
-        color="transparent"
-        className={styles.title}
-      >
-        Quest Analytics
-      </Typography>
       <div className={styles.card_container}>
         {loading ? (
           <FeaturedQuestSkeleton />
