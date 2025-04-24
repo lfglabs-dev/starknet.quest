78d2e5b4a923b31e3b837bf63b17b177e14bbbab
Ifechi <ifechi.dev@gmail.com>
2025-04-24 23:31:37 +0100

    Fix: Add Lending Protocols category cards

--- app/discover/defi/page.tsx
+++ app/discover/defi/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import React, { useEffect, useCallback } from "react";
 import DataTable from "@components/discover/defiTable";
@@ -14,6 +14,7 @@ import {
   getPairingStats,
 } from "@services/apiService";
 import { formatStatsData } from "@utils/defi";
+import { LendingAndBorrowing } from "./categories";
 
 export default function Page() {
   const [data, setData] = React.useState<TableInfo[]>([]);
@@ -46,28 +47,31 @@ export default function Page() {
   }, []);
 
   return (
-    <div className="flex w-full flex-col mt-24 gap-8 items-center mb-32">
-      <div className="mx-4 p-6  rounded-xl w-full lg:w-3/4 mb-36">
+    <div className="flex flex-col items-center w-full gap-8 mt-24 mb-32">
+      <div className="w-full p-6 mx-4 rounded-xl lg:w-3/4 mb-36">
         <DataTable loading={loading} data={data} />
       </div>
 
-      <div className="w-full lg:w-3/4 px-5 lg:px-0 relative">
+      <div className="relative w-full px-5 lg:w-3/4 lg:px-0">
         <div className="absolute -right-1/2 top-0 w-[781px] h-[764px] opacity-30 pointer-events-none">
           <Image
             src="/icons/patternCircle.svg"
             alt="pattern-circle"
             width={781}
             height={764}
-            className="w-full h-full relative animate-spin-slow"
+            className="relative w-full h-full animate-spin-slow"
           />
         </div>
 
-        <div className="max-w-sm md:max-w-none mx-auto">
-          <Typography type={TEXT_TYPE.H2} className="mb-8 text-center lg:text-left text-white font-bold text-2xl">
+        <div className="max-w-sm mx-auto md:max-w-none">
+          <Typography
+            type={TEXT_TYPE.H2}
+            className="mb-8 text-2xl font-bold text-center text-white lg:text-left"
+          >
             Essential DeFi Concepts
           </Typography>
           <div
-            className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 relative z-5"
+            className="relative grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 z-5"
             aria-label="DeFi concept cards grid"
           >
             {DEFI_CONCEPTS.map((concept) => (
@@ -79,6 +83,10 @@ export default function Page() {
               />
             ))}
           </div>
+        </div>
+
+        <div className="w-full">
+          <LendingAndBorrowing />
         </div>
       </div>
     </div>
