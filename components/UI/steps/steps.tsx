817511b1e875c915f88c7e173984fc31c88f37d6
Sneha Gupta <96808735+Snehagupta1907@users.noreply.github.com>
2025-05-09 07:00:51 +0000

    update

--- components/UI/steps/steps.tsx
+++ components/UI/steps/steps.tsx
@@ -13,7 +13,7 @@ const Steps: FunctionComponent<StepsProps> = ({
   subTitleBefore = false,
   steps,
 }) => {
-  console.log({steps})
+
   return (
     <div className={styles.container}>
       <div className={styles.cards}>
