67ba113429152d2a683b6600379ab262ad01a827
Nico <60229704+Marchand-Nicolas@users.noreply.github.com>
2025-05-22 09:45:00 +0200

    fix responsive

--- components/UI/DefiDiscoverCard.tsx
+++ components/UI/DefiDiscoverCard.tsx
@@ -19,9 +19,9 @@ export default function DefiDiscoverCard({
       href={link}
       target="_blank"
       rel="noopener noreferrer"
-      className="no-underline"
+      className="no-underline flex items-center justify-center"
     >
-      <div className="relative bg-gray-300 w-[176px] h-[160px] p-4 rounded-lg flex flex-col items-center justify-center text-center transition-all duration-300 ease-in-out hover:transform hover:-translate-y-1 hover:bg-[var(--hover-background-color)] hover:shadow-lg hover:shadow-[#ffffff0d] cursor-pointer">
+      <div className="relative bg-gray-300 w-min-[150px] w-full max-w-[176px] h-[160px] p-4 rounded-lg flex flex-col items-center justify-center text-center transition-all duration-300 ease-in-out hover:transform hover:-translate-y-1 hover:bg-[var(--hover-background-color)] hover:shadow-lg hover:shadow-[#ffffff0d] cursor-pointer">
         <FaExternalLinkAlt className="absolute top-3 right-3 text-[#E1DCEA] m-1 text-xs" />
         <div className="mb-5 rounded-full overflow-hidden">
           <Image
