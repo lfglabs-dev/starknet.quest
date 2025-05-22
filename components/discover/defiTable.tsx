e2e709de84259d66de4fd9919506cde13abcf7a7
pheobeayo <pheobeayo@gmail.com>
2025-05-22 11:01:22 +0100

    fix changes requested

--- components/discover/defiTable.tsx
+++ components/discover/defiTable.tsx
@@ -147,10 +147,10 @@ export const columns: ColumnDef<TableInfo>[] = [
               );
             }}
           >
-              {/* Token Icons Section */}
-            <div className="flex items-center -space-x-1"> 
+              {/* Token Icons Section with Overlap */}
+            <div className="flex items-center"> 
               {token1Icon && (
-                <div className="relative w-6 h-6 bg-white rounded-full p-0.5 border border-gray-200">
+                <div className="relative w-6 h-6 bg-transparent rounded-full p-0.5 border border-gray-200 z-10">
                   <CDNImg 
                     src={token1Icon} 
                     width={20} 
@@ -161,7 +161,7 @@ export const columns: ColumnDef<TableInfo>[] = [
                 </div>
               )}
               {token2Icon && (
-                <div className="relative w-6 h-6 bg-white rounded-full p-0.5 border border-gray-200">
+                <div className="relative w-6 h-6 bg-transparent rounded-full p-0.5 border border-gray-200 -ml-2">
                   <CDNImg 
                     src={token2Icon} 
                     width={20} 
