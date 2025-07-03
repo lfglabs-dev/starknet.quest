43492ec60c911efbc6ce20c8aa1cd711c6057735
Luis Carrión <louiss862@gmail.com>
2025-07-03 16:40:37 -0300

    feat: add image to Stats component

--- components/UI/stats/stats.tsx
+++ components/UI/stats/stats.tsx
@@ -3,6 +3,7 @@ import styles from "@styles/components/stats.module.css";
 import StatElement from "./statElement";
 import Typography from "../typography/typography";
 import { TEXT_TYPE } from "@constants/typography";
+import Image from "next/image";
 
 type StatsProps = {
   title?: string;
@@ -16,7 +17,7 @@ const Stats: FunctionComponent<StatsProps> = ({ title, stats }) => {
   return (
     <div className={styles.container}>
       {title && <Typography type={TEXT_TYPE.H2} color="secondary" className={styles.title}>{title}</Typography>}
-      <div className={styles.elementsContainer}>
+      <div className="flex max-lg:flex-col gap-4">
         {stats.map((elt, index) => (
           <StatElement
             key={`stats_${index}`}
@@ -24,6 +25,11 @@ const Stats: FunctionComponent<StatsProps> = ({ title, stats }) => {
             value={elt.value}
           />
         ))}
+      <Image src="/tokens.png"
+        height={260}
+        width={294}
+        alt="Tokens"
+      />
       </div>
     </div>
   );
